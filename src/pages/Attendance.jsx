83cc9d909906e408937a63aac2fc5837deb66db1
vivexatech-code <vivexatech@gmail.com>
2026-01-18 14:18:37 +0530

    first commit

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -220,7 +220,7 @@ export default function Attendance() {
         setShowCheckIn(false);
     };
     return (
-        <>
+        <> 
             <Header />
             <CalendarProps />
             <div className="attendance-card">
