4690a01629b6be9fa6021ad34203419a95694bbd
vivexatech-code <vivexatech@gmail.com>
2026-01-18 15:26:56 +0530

    first commit

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import CalendarProps from "../components/CalendarProps";
 import Header from "../components/Header";
 import React, { useEffect, useState } from "react";
